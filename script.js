5fde81f3ad68cfce8c407d198a1580d30d103c72
Syed Talha <syedt.in00@gmail.com>
2023-12-09 12:53:43 +0500

    files added to repo

---
+++ script.js
@@ -0,0 +1,37 @@
+// script.js
+let timer;
+let timeLeft;
+let isPaused = false;
+
+function startTimer(minutes) {
+    if (timer) {
+        clearInterval(timer);
+    }
+
+    timeLeft = minutes * 60;
+    updateTimerDisplay();
+
+    timer = setInterval(function() {
+        if (!isPaused) {
+            timeLeft--;
+            updateTimerDisplay();
+
+            if (timeLeft <= 0) {
+                clearInterval(timer);
+                alert('Time is up!');
+            }
+        }
+    }, 1000);
+}
+
+function updateTimerDisplay() {
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft % 60;
+    document.getElementById('timer').innerText = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+}
+
+function pauseTimer() {
+    isPaused = !isPaused;
+    const pauseButton = document.getElementById('pauseButton');
+    pauseButton.innerText = isPaused ? 'Resume' : 'Pause';
+}
